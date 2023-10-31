4ddddcfffc6f84e229bea95a219979b5989cca63
Jiraf400 <109845546+Jiraf400@users.noreply.github.com>
2023-10-31 16:28:53 +0300

    add register validation mware to router

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import { registerUser, loginUser } from '../controllers/authController.js';
+import { validateRegisterFields } from '../middlewares/registerFieldsValidation.js';
 
 export const authRouter = Router();
 
-authRouter.post('/register', registerUser);
+authRouter.post('/register', validateRegisterFields, registerUser);
 authRouter.post('/login', loginUser);
