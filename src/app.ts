f6787ccd2cfb479a490a61ec819b46ad4b4cd9c7
Jiraf400 <109845546+Jiraf400@users.noreply.github.com>
2023-11-01 18:52:23 +0300

    move middleware logic from app.ts to auth router

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express';
 import { authRouter } from './routers/authRouter.js';
+import { orderRouter } from './routers/orderRouter.js';
 
 const app: Express = express();
 const port = 5000;
@@ -7,6 +8,7 @@ const port = 5000;
 app.use(express.json());
 
 app.use('/auth', authRouter);
+app.use('/orders', orderRouter);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 import { registerUser, loginUser } from '../controllers/authController.js';
-import { validateRegisterFields } from '../middlewares/registerFieldsValidation.js';
+import { validateAuthenticationFields } from '../middlewares/authFieldsValidation.js';
 
 export const authRouter = Router();
 
-authRouter.post('/register', validateRegisterFields, registerUser);
-authRouter.post('/login', loginUser);
+authRouter.post('/register', validateAuthenticationFields, registerUser);
+authRouter.post('/login', validateAuthenticationFields, loginUser);
