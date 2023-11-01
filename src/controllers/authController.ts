e17d08967ed38e097f1db81459525bf8816e10ce
Jiraf400 <109845546+Jiraf400@users.noreply.github.com>
2023-11-01 20:03:13 +0300

    add global error handling

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,9 +2,19 @@ import { register, login } from '../services/authentication.js';
 import { Request, Response } from 'express';
 
 export const registerUser = (req: Request, res: Response) => {
-  return register(req, res);
+  try {
+    return register(req, res);
+  } catch (err: any) {
+    console.error(err);
+    return res.status(400).json({ status: 400, message: err.message });
+  }
 };
 
 export const loginUser = (req: Request, res: Response) => {
-  return login(req, res);
+  try {
+    return login(req, res);
+  } catch (err: any) {
+    console.error(err);
+    return res.status(400).json({ status: 400, message: err.message });
+  }
 };
