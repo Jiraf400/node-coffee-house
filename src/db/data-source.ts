f6adc77b21757936e5722339819ab332f34f37e6
Jiraf400 <109845546+Jiraf400@users.noreply.github.com>
2023-11-02 18:13:11 +0300

    update data source file. Add new schemas

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -1,23 +1,25 @@
-import "reflect-metadata"
-import dotenv from "dotenv";
-import {DataSource} from "typeorm"
-import {User} from '../models/User.js'
-import {Card} from "../models/Card.js";
+import 'reflect-metadata';
+import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+import { User } from '../models/User.js';
+import { Card } from '../models/Card.js';
+import { Order } from '../models/Order.js';
+import { MenuItem } from '../models/Menu-Item.js';
 
 dotenv.config();
 
 const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "javauser",
-    password: POSTGRES_PASSWORD,
-    database: "coffee_house",
-    synchronize: true,
-    logging: false,
-    entities: [User, Card],
-    migrations: [],
-    subscribers: [],
-})
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'javauser',
+  password: POSTGRES_PASSWORD,
+  database: 'coffee_house',
+  synchronize: true,
+  logging: false,
+  entities: [User, Card, Order, MenuItem],
+  migrations: [],
+  subscribers: [],
+});
