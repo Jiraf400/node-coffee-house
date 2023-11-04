c21161693f095dfc6b8c85d0e5a41517925eb278
Jiraf400 <109845546+Jiraf400@users.noreply.github.com>
2023-11-04 15:29:31 +0300

    move middlewares to main ts file. Provide error handling

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
 import express, { Express } from 'express';
 import { authRouter } from './routers/authRouter.js';
 import { orderRouter } from './routers/orderRouter.js';
+import { validateAuthenticationFields } from './middlewares/authFieldsValidation.js';
+import { validateJwtToken } from './middlewares/jwtTokenValidation.js';
+import { errorHandler } from './middlewares/errors.js';
+import 'express-async-errors';
 
 const app: Express = express();
 const port = 5000;
 
 app.use(express.json());
 
-app.use('/auth', authRouter);
-app.use('/orders', orderRouter);
+app.use('/auth', validateAuthenticationFields, authRouter);
+app.use('/orders', validateJwtToken, orderRouter);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
